5d31a19d1cb3952602959fc61fa19767cf2fe3b9
radwaalkrargy <162440007+radwaalkrargy@users.noreply.github.com>
2025-05-10 13:05:21 +0300

    Update Zakat_Calc.java

--- src/Zakat_Calc.java
+++ src/Zakat_Calc.java
@@ -1,15 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zakat_Calc {
+    private static final double NISAB = 5000;
     private double zakat_rate;
 
-    Zakat_Calc( double zakat_rate){
-        this.zakat_rate = zakat_rate;
+    public Zakat_Calc( double zakat){
+        this.zakat_rate = zakat;
     }
 
-    double calculateZakat(){
+    public double calculateZakat(Portfolio portfolio){
+        List<Asset> liquidInvestment = new ArrayList<>();
+        liquidInvestment = portfolio.getLiquidAssets();
+        System.out.println("------------------------------------------------------------------------");
+        System.out.printf("%-5s %-15s %-15s %-10s%n", "" ,"Asset Name", "Asset Value" ,"Zakat Amount");
+        for (Asset asset : liquidInvestment) {
+            if (isZakatApplicable(asset)) {
+                double zakat = asset.getValue() * zakat_rate;
+                System.out.printf("%-5s %-17s %-16s %-10s%n", "" ,asset.getName() , asset.getValue() , zakat);
+            }
+            else{
+                System.out.printf("%-5s %-15s %-15s %-10s%n", asset.getName() , "not applicable");
+            }
+
+        }
+        System.out.println("------------------------------------------------------------------------");
         return 0;
     }
 
-    boolean isZakatApplicable (){
-        return false;
+    public boolean isZakatApplicable (Asset asset){
+        return asset.getValue() >= NISAB && asset.getHoldingYears() >= 1 && asset.isHalal() ;
     }
+
+
 }
