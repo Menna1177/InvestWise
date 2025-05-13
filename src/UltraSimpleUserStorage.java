a9a6385af02115b97190b1aca77179cc0580372a
z18409 <159724341+z18409@users.noreply.github.com>
2025-05-14 00:24:37 +0300

    Update UltraSimpleUserStorage.java

--- src/UltraSimpleUserStorage.java
+++ src/UltraSimpleUserStorage.java
@@ -11,6 +11,13 @@ public class UltraSimpleUserStorage {
         private String username;
         private String password;
 
+        /**
+         *
+         * @param name
+         * @param email
+         * @param username
+         * @param password
+         */
         public void register(String name, String email, String username, String password) {
             this.id = ++user_id;
             this.name = name;
@@ -19,14 +26,28 @@ public class UltraSimpleUserStorage {
             this.password = password;
         }
 
+        /**
+         *
+         * @param username
+         * @param password
+         * @return
+         */
         public boolean authenticate(String username, String password) {
             return username != null && username.equals(this.username) && password != null && password.equals(this.password);
         }
 
+        /**
+         *
+         * @return
+         */
         // Getters
         public int getId() { return id; }
         public String getUsername() { return username; }
 
+        /**
+         *
+         * @return
+         */
         @Override
         public String toString() {
             return String.format("User{id=%d, name='%s', email='%s'}", id, name, email);
